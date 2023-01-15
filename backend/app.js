dec606f5e06f58b0acd0348a1db5b5dbbf2df681
Suneelkuma <sunil24195@gmail.com>
2023-01-15 07:35:56 +0530

    removed authentication from app

--- backend/app.js
+++ backend/app.js
@@ -15,32 +15,32 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Ok");
 });
-app.use("/api/v1/posts", (req, res, next) => {
-    if (req.headers.authorization) {
-        const token = req.headers.authorization;
-        console.log("Verify token");
-        if (token) {        // verify a token symmetric
-            jwt.verify(token, secret, function (err, decoded) {
-                if(err){
-                    return res.status(403).json({
-                        status: "failed",
-                        message: "Invalid token"
-                    })
-                }
-                req.user = decoded.data;
-                next();
-            });
-        } else {
-            return res.status(403).json({
-                status: "failed",
-                message: "Invalid token"
-            })
-        }
-    }else {
-        return res.status(403).json({ status: "Failed", 
-        message : "Not authenticated user"});
-    }
-})
+// app.use("/api/v1/posts", (req, res, next) => {
+//     if (req.headers.authorization) {
+//         const token = req.headers.authorization;
+//         console.log("Verify token");
+//         if (token) {        // verify a token symmetric
+//             jwt.verify(token, secret, function (err, decoded) {
+//                 if(err){
+//                     return res.status(403).json({
+//                         status: "failed",
+//                         message: "Invalid token"
+//                     })
+//                 }
+//                 req.user = decoded.data;
+//                 next();
+//             });
+//         } else {
+//             return res.status(403).json({
+//                 status: "failed",
+//                 message: "Invalid token"
+//             })
+//         }
+//     }else {
+//         return res.status(403).json({ status: "Failed", 
+//         message : "Not authenticated user"});
+//     }
+// })
 app.use("/api/v1", loginRoutes);
-app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1", postRoutes);
 app.listen(PORT, () => console.log(`The server is up at  ${PORT}`));
