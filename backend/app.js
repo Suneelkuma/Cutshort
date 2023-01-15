1e10e989c17c3ba5bed5993904521620a1623e43
Suneelkuma <sunil24195@gmail.com>
2023-01-15 09:00:46 +0530

    removed authorization from app

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 var jwt = require('jsonwebtoken');
 const loginRoutes = require("./routes/login");
 const postRoutes = require("./routes/post");
+const todosRoutes = require("./routes/todo");
 dotenv.config({ path: "./config.env" });
 require('./db/connection')
 const PORT = process.env.PORT;
@@ -15,32 +16,8 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Ok");
 });
-// app.use("/api/v1/posts", (req, res, next) => {
-//     if (req.headers.authorization) {
-//         const token = req.headers.authorization;
-//         console.log("Verify token");
-//         if (token) {        // verify a token symmetric
-//             jwt.verify(token, secret, function (err, decoded) {
-//                 if(err){
-//                     return res.status(403).json({
-//                         status: "failed",
-//                         message: "Invalid token"
-//                     })
-//                 }
-//                 req.user = decoded.data;
-//                 next();
-//             });
-//         } else {
-//             return res.status(403).json({
-//                 status: "failed",
-//                 message: "Invalid token"
-//             })
-//         }
-//     }else {
-//         return res.status(403).json({ status: "Failed", 
-//         message : "Not authenticated user"});
-//     }
-// })
+
 app.use("/api/v1", loginRoutes);
 app.use("/api/v1", postRoutes);
+app.use("/api/v1", todosRoutes);
 app.listen(PORT, () => console.log(`The server is up at  ${PORT}`));
