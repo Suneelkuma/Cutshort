fad4e8cde8275748b5469cbf78546553ee4d67b7
Suneelkuma <sunil24195@gmail.com>
2023-01-16 12:28:28 +0530

    list of api using which can be checked

---
+++ backend/listofapi.js
@@ -0,0 +1,12 @@
+// 1.to register a user
+// 1.  http://localhost:5000/api/v1/register
+
+// 2.to login a user
+// http://localhost:5000/api/v1/login
+
+// 3.to create apost  (all operation can be done get post put delete)
+// http://localhost:5000/api/v1/posts
+
+// 4.to create todos (all operation can be done get post put delete)
+
+// http://localhost:5000/api/v1/todos
